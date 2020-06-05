9a7fc5aedb5f4ecb233e18f31fa7a019896c46b0
lina.sok <lina.sok@prasac.com.kh>
2020-06-05 15:08:24 +0700

    first commit

---
+++ wp-content/litespeed/cssjs/0fec3.js
@@ -0,0 +1,1 @@
+(function($){"use strict";$(window).on('load resize',function(){var viewportWidth=window.outerWidth;;var siteHeader=$('.site-header');var isMobile=siteHeader.hasClass('mobile');if(viewportWidth<1008){if(!isMobile){siteHeader.addClass('mobile');$('body').addClass('mobile');}}else{if(isMobile){siteHeader.removeClass('mobile');$('body').removeClass('mobile');}}});})(jQuery);;
---
+++ wp-content/litespeed/cssjs/329cc.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$(document).on('click','.search-adv-reset',function(){$('input:text').val('');$('input:checkbox').attr('checked',false);$('input:radio').attr('checked',false);return false;});$(document).on('click','#manga-search-adv',function(){$('#search-advanced').toggleClass('collapse');return false;});});
